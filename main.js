c3e7ba46a932945603036c1a91ef3b104e091b7b
Leia <leiahopf@googlemail.com>
2020-05-23 10:16:26 +0100

    finished!

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
 const full = document.querySelector('.full');
-const empty = document.querySelectorAll('.empty');
+const empties = document.querySelectorAll('.empty');
+
+//EventListeners
+full.addEventListener('dragstart', dragStart);
+full.addEventListener('dragend', dragEnd);
+
+//Loop through Empties
+for(const empty of empties){
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+//Functions (Full)
+function dragStart(){
+  setTimeout(() => this.className = 'invisible', 0);
+}
+
+function dragEnd(){
+  this.className = 'full';  
+}
+
+//Functions (Empties)
+function dragOver(event){
+    event.preventDefault();  
+}
+
+function dragEnter(event){
+    event.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave(){
+    this.className = 'empty';
+}
+
+function dragDrop(){
+    this.className = 'empty';
+    this.append(full);  
+}
